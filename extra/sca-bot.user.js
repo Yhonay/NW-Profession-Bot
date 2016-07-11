8b4fdc8411e39e36b35b51c2d092d08716beea45
Vetyt Yhonay <yhonay@gmail.com>
2016-07-11 19:45:18 +0200

    [sca bot] pause bot when not in focus

--- extra/sca-bot.user.js
+++ extra/sca-bot.user.js
@@ -469,7 +469,7 @@ try {
 
       var args = Array.prototype.slice.call(arguments, 1);
       var state = args[0];
-      var passing = PAUSED || !(state in eventHandlers);
+      var passing = PAUSED || !(state in eventHandlers) || window.location.href.indexOf('/adventures') == -1;
 
       debug(2, 'scaProcessState', args, 'passing: ', passing);
 
