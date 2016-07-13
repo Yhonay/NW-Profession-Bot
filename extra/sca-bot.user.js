46317587a6fdd366df19f793b94919d797821df9
Vetyt Yhonay <yhonay@gmail.com>
2016-07-13 08:45:46 +0200

    [sca bot] longer timeout
    processing professions can take a lot of time

--- extra/sca-bot.user.js
+++ extra/sca-bot.user.js
@@ -553,7 +553,7 @@ try {
     var lastAction = new Date();
 
     function sleeping() {
-      var dur = 60 * 1000; // 1min
+      var dur = 60 * 1000 * 2; // 2min
       var diff = (new Date() - lastAction);
       return diff > dur && window.location.href.indexOf('adventures') != -1;
     }
